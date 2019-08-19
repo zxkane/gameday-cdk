b5b79145332db68fd306ec7687414f9578e31e56
Kane Zhu <kane.mx@gmail.com>
2019-08-19 15:26:47 +0800

    Configure VPC.

--- lib/gameday-cdk-stack.ts
+++ lib/gameday-cdk-stack.ts
@@ -1,9 +1,29 @@
 import cdk = require('@aws-cdk/core');
+import ec2 = require("@aws-cdk/aws-ec2");
+import { SubnetType } from '@aws-cdk/aws-ec2'
 
 export class GamedayCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'Gameday', {
+      cidr: '10.0.0.0/16',
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      maxAzs: 2,
+      subnetConfiguration: [ 
+        { 
+          cidrMask: 24, 
+          name: 'Public', 
+          subnetType: SubnetType.PUBLIC
+        }, 
+        { 
+          cidrMask: 24, 
+          name: 'Private', 
+          subnetType: SubnetType.PRIVATE
+        }
+      ]
+    });
   }
 }
